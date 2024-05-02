d30b041d40490722ee343cf6d9e3840c5bb76fbb
Liudmila <tsiarmila@gmail.com>
2024-05-02 13:34:32 +0200

    useNewUrlParser added

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use(cors());
 // dotenv.config({ path: 'C:\\data\\.env' });
 // dotenv.config({ path: path.resolve(__dirname, 'data', '.env') });
 // const USER_NAME = process.env.MONGOATLASUSERNAME;
-const PASSWORD = process.env['MONGOATLASPASSWORD'];
+const PASSWORD = process.env.MONGOATLASPASSWORD;
 
-mongoose.connect(`mongodb+srv://admin-milatsiar:${PASSWORD}@cluster0.yhdmafz.mongodb.net/todoDB`);
+mongoose.connect(`mongodb+srv://admin-milatsiar:${PASSWORD}@cluster0.yhdmafz.mongodb.net/todoDB`, { useNewUrlParser: true});
 
 userSchema = new mongoose.Schema({
   name: String,
